d218cbdd12afd4585a61bede76efa468c3981dfb
Steffen Mogensen <feber.sm@gmail.com>
2015-11-04 20:37:46 +0100

    check-in

---
+++ app.py
@@ -0,0 +1,37 @@
+#!/usr/python
+import pymssql
+import sql
+
+# Resets the database
+def reset_db(host, user, password, database):
+    print '[*] Connecting to db: %s' % database
+    conn = pymssql.connect(host=host, user=user, password=password, database=database)
+    cur = conn.cursor()
+
+    print '[*] Executing sql script'
+    cur.execute(sql.sql_query)
+    conn.commit()
+
+    conn.close()
+    print '[+] Complete!\n'
+
+
+# Main function
+def run():
+    host = 'HOST_ADDRESS'
+    user = 'USER'
+    password = 'PASSWORD'
+    database = "DATABASE"
+
+
+    count = 0
+
+    # Chance 0 to the amount of times to loop
+    while (count < 0):
+        reset_db(host, user+count, password+count, database)
+        count = count + 1
+
+    print '[*] Finished'
+
+# Start the script
+run()
